f6d17d21d02d52871358767f312926fdfd96f560
Galib <60702760+galib-i@users.noreply.github.com>
2024-10-05 21:04:39 +0100

    Set up using sample

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+from google.cloud import vision
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
+
+
+def main():
+    detect_document("test.jpg")
+
+
+def detect_document(path):
+    """Detects document features in an image."""
+
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, "rb") as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.document_text_detection(image=image)
+
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            print(f"\nBlock confidence: {block.confidence}\n")
+
+            for paragraph in block.paragraphs:
+                print("Paragraph confidence: {}".format(paragraph.confidence))
+
+                for word in paragraph.words:
+                    word_text = "".join([symbol.text for symbol in word.symbols])
+                    print(
+                        "Word text: {} (confidence: {})".format(
+                            word_text, word.confidence
+                        )
+                    )
+
+                    for symbol in word.symbols:
+                        print(
+                            "\tSymbol: {} (confidence: {})".format(
+                                symbol.text, symbol.confidence
+                            )
+                        )
+
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
+        )
+
+
+main()
