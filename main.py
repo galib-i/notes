f483fad37919e205bde21f79381117ba60d42c95
Galib <60702760+galib-i@users.noreply.github.com>
2024-10-09 21:56:38 +0100

    Add by line

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from google.cloud import vision
 PATH = "images\\b.jpg"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
 
+
 def main():
     img_content = pre_process_image(PATH)
     detect_document(img_content)
 
+
 def pre_process_image(image_path):
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # Read image directly in grayscale
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # read image directly in grayscale
 
     max_height = 1024
     if image.shape[0] > max_height:
@@ -32,17 +34,40 @@ def detect_document(image_content):
     if response.error.message:
         raise Exception(f"{response.error.message}\nFor more info on error messages, check: https://cloud.google.com/apis/design/errors")
 
-    text_annotations = response.text_annotations
-    if text_annotations:
-        words_with_coords = []
-        for annotation in text_annotations[1:]:  # Skip the first annotation as it is the full text
-            word = annotation.description
-            vertices = annotation.bounding_poly.vertices
-            top_left_x = vertices[0].x
-            top_left_y = vertices[0].y
-            words_with_coords.append(f"{word} ({top_left_x}, {top_left_y})")
-        
-        full_text_with_coords = " ".join(words_with_coords)
-        print(full_text_with_coords)
+    if text_annotations := response.text_annotations:
+        post_process_text(text_annotations)
+
+
+def post_process_text(text_annotations):
+    processed_words = post_process_word(text_annotations)
+    print(processed_words)
+    full_text = text_annotations[0].description
+    lines = full_text.split('\n')
+
+    processed_lines = []
+    for line in lines:
+        if line and line[0].islower() and processed_lines:
+            processed_lines[-1] += f' {line}'
+        else:
+            processed_lines.append(line)
+    #print(processed_lines)
+    final_text = '\n'.join(processed_lines)
+    #print(final_text)
+
+
+def post_process_word(text_annotations):
+    words_with_coords = []
+
+    for annotation in text_annotations[1:]:  # skip the first annotation as it is the full text
+        word = annotation.description
+        vertices = annotation.bounding_poly.vertices
+        top_left_x = vertices[0].x
+        top_left_y = vertices[0].y
+
+        word = f"{word} ({top_left_x}, {top_left_y})"
+
+        words_with_coords.append(word)
+
+    return words_with_coords
 
 main()
